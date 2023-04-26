7f524e3e32cfcc1acae217309969d42411f544cb
Viktorhs <victor.hugo.gba@hotmail.com.br>
2023-04-26 15:14:14 -0300

    invertString

---
+++ InvertString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InvertString
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite uma palavra: ");
+      string palavra = Console.ReadLine();
+      int tamanho = palavra.Length;
+
+      char[] arrayCaracteres = new char[tamanho];
+
+      for (int i = 0; i < tamanho; i++)
+      {
+        arrayCaracteres[i] = palavra[i];
+      }
+
+      for (int i = 0; i < tamanho / 2; i++)
+      {
+        char temp = arrayCaracteres[i];
+        arrayCaracteres[i] = arrayCaracteres[tamanho - i - 1];
+        arrayCaracteres[tamanho - i - 1] = temp;
+      }
+
+      string palavraInvertida = new string(arrayCaracteres);
+      Console.WriteLine($"A palavra invertida é: {palavraInvertida}");
+    }
+  }
+}
