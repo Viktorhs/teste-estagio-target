8a826c23d2cc54f83da3084b6b9086157060ab95
Viktorhs <victor.hugo.gba@hotmail.com.br>
2023-04-26 15:13:30 -0300

    2-Fibonacci

---
+++ 2-Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Digite um número inteiro: ");
+      int num = int.Parse(Console.ReadLine()); // informar o numero pelo console.
+      // int num = 0; //se preferir tire o comentario da linha e comente a anterior e insira o valor desejado na variavel.
+
+      int fib1 = 0;
+      int fib2 = 1;
+
+      while (fib2 < num)
+      {
+        int temp = fib2;
+        fib2 = fib1 + fib2;
+        fib1 = temp;
+      }
+
+      if (fib2 == num)
+      {
+        Console.WriteLine($"O número {num} pertence à sequência de Fibonacci.");
+      }
+      else
+      {
+        Console.WriteLine($"O número {num} não pertence à sequência de Fibonacci.");
+      }
+    }
+  }
+}
